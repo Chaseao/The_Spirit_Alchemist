81e0aaf054d4be1cb5ffb52f2e68a99ac1cf588b
Chaseao <chaseao.oberg@gmail.com>
2022-01-31 23:01:14 -0500

    Music, Credits, and final touches

--- Assets/EndCreditsController.cs
+++ Assets/EndCreditsController.cs
@@ -41,4 +41,9 @@ public class EndCreditsController : MonoBehaviour
         thankYou.enabled = true;
     }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
--- Assets/SceneSwitch.cs
+++ Assets/SceneSwitch.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using PixelCrushers.DialogueSystem;
 
 public class SceneSwitch : MonoBehaviour
 {
     public void SwitchSceneToCredits()
     {
-        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
+        DialogueManager.StopConversation();
+        StartCoroutine(Delay(2));
+    }
+    
+    private IEnumerator Delay(int delayAmount)
+    {
+        yield return new WaitForSeconds(delayAmount);
+        SceneManager.LoadScene(1);
     }
 }
